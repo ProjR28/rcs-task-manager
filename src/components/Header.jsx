2d10323a04880ef943ff927153d4f2ef8b16f477
Rafael Correa <projetos.rafael.c@gmail.com>
2026-03-27 11:02:45 -0300

    chore: add tailwind prettier plugin

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <header>
-      <h1 className="text-lime-600 pt-4">Gerenciador de Tarefas</h1>
+      <h1 className="mt-2 pt-4 text-lime-600">Gerenciador de Tarefas</h1>
     </header>
   )
 }
