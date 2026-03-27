c47c93bf855ed0c4f2dc38c7121414948e09fff0
Rafael Correa <projetos.rafael.c@gmail.com>
2026-03-27 10:39:49 -0300

    chore: add tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <h1>Gerenciador de Tarefas</h1>
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <header>
-      <h1>Gerenciador de Tarefas</h1>
+      <h1 className="text-lime-600 pt-4">Gerenciador de Tarefas</h1>
     </header>
   )
 }
