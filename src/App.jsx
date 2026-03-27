72d0abf088cc2fbeab1dd9e9207ffd6f6eea8a1d
Rafael Correa <rafael_dev@PC-RAFAEL.>
2026-03-27 09:06:27 -0300

    chore: add eslint and prettier setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import Header from './components/Header'
+
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Gerenciador de Tarefas</h1>
     </div>
   )
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header>
+      <h1>Gerenciador de Tarefas</h1>
+    </header>
+  )
+}
+
+export default Header
