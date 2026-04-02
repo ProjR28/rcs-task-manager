176b574ca0c314081b2cee32418519a3c86bf807
Rafael Correa <projetos.rafael.c@gmail.com>
2026-04-02 09:50:09 -0300

    feat: add svg icons to Sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,12 +1,12 @@
 import SidebarButton from './SidebarButton'
+import HomeIcon from '../assets/icons/home.svg?react'
+import TaskIcon from '../assets/icons/tasks.svg?react'
 
 const Sidebar = () => {
   return (
     <div className="h-screen w-64 bg-white">
       <div className="space-y-4 px-8 py-6">
-        <h1 className="text-xl font-semibold text-blue-600">
-          Gerenciador de Tarefas
-        </h1>
+        <h1 className="text-xl font-semibold text-blue-600">Task Manager</h1>
         <p className="text-sm text-gray-500">
           Um simples{' '}
           <span className="text-blue-600">gerenciador de tarefas</span>
@@ -14,8 +14,14 @@ const Sidebar = () => {
       </div>
 
       <div className="flex flex-col gap-2 p-2">
-        <SidebarButton variant={'unselected'}>Início</SidebarButton>
-        <SidebarButton variant={'selected'}>Minhas Tarefas</SidebarButton>
+        <SidebarButton variant={'unselected'}>
+          <HomeIcon />
+          Início
+        </SidebarButton>
+        <SidebarButton variant={'selected'}>
+          <TaskIcon />
+          Minhas Tarefas
+        </SidebarButton>
       </div>
     </div>
   )
